26386ac9d6c59c8b95e0e7c39348dcbea16b231f
bruxromuar <brux.romuar@gmail.com>
2016-03-29 12:21:29 +0800

    add projects

--- sirius/projects/projects.py
+++ sirius/projects/projects.py
@@ -1,30 +1,30 @@
 from models import Project
-from flask import Blueprint, abort, redirect, render_template, url_for
+from flask import Blueprint, abort, redirect, render_template, request, url_for
 
 app = Blueprint('projects', __name__,
                 template_folder='templates')
 
-@app.route('/')
+@app.route('/projects')
 def index():
     projects = Project.get_all()
     return render_template('index.html', projects=projects)
 
-@app.route('/', methods=['POST'])
+@app.route('/projects', methods=['POST'])
 def create():
-    name = self.request.get('name').strip()
+    name = request.form['name'].strip()
     if (name == ''): abort(400)
 
-    url = self.request.get('url').strip()
+    url = request.form['url'].strip()
     if (url == ''): abort(400)
 
-    desc = self.request.get('desc').strip()
+    desc = request.form['desc'].strip()
 
     project = Project(name=name, url=url, desc=desc)
     project.put()
 
-    redirect(url_for('.show'))
+    redirect('/projects/' + str(project.key.id()))
 
-@app.route('/<int:project_id>', methods=['GET'])
+@app.route('/projects/<int:project_id>', methods=['GET'])
 def show(project_id):
     project = Project.get_by_id(long(project_id))
     if (project == None):
@@ -32,10 +32,10 @@ def show(project_id):
     else:
         return render_template('show.html', project=project)
 
-@app.route('/<int:project_id>', methods=['PATCH'])
+@app.route('/projects/<int:project_id>', methods=['PATCH'])
 def update(project_id):
     abort(503)
 
-@app.route('/<int:project_id>', methods=['DELETE'])
-def update(project_id):
+@app.route('/projects/<int:project_id>', methods=['DELETE'])
+def destroy(project_id):
     abort(503)
