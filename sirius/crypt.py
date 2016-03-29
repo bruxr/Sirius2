59058fec978d5a60d614bd6aea8467bdadd775e3
bruxromuar <brux.romuar@gmail.com>
2016-03-29 14:01:56 +0800

    add encryption util

---
+++ sirius/crypt.py
@@ -0,0 +1,24 @@
+import os
+import base64
+from Crypto import Random
+from Crypto.Cipher import AES
+
+key = os.environ.get('APP_KEY')
+
+def encrypt(raw):
+    raw = pad(raw)
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return base64.b64encode(iv + cipher.encrypt(raw))
+
+def decrypt(enc):
+    enc = base64.b64decode(enc)
+    iv = enc[:AES.block_size]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return unpad(cipher.decrypt(enc[AES.block_size:]))
+
+def pad(s):
+    return s + (16 - len(s) % 16) * chr(16 - len(s) % 16)
+
+def unpad(s):
+    return s[:-ord(s[len(s)-1:])]
