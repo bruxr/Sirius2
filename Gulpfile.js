d407d5cfaa64312c75a990e49428cacfae67658e
bruxromuar <brux.romuar@gmail.com>
2016-03-30 13:42:51 +0800

    add frontend build system

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,3 +1,5 @@
+var babelify = require('babelify');
+var browserify = require('browserify');
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var fs = require('fs-extra');
@@ -11,4 +13,13 @@ gulp.task('bootstrap', function() {
         'node_modules/bootstrap/dist/css/bootstrap-theme.css'
     ]).pipe(concat('bootstrap.css'))
       .pipe(gulp.dest('public/assets'));
+});
+
+gulp.task('scripts', function() {
+    return browserify({ debug: true })
+        .transform(babelify)
+        .require('./sirius/assets/javascripts/main.js', { entry: true })
+        .bundle()
+        .on('error', function(err) { console.log('Error: ' + err.message) })
+        .pipe(fs.createWriteStream('public/assets/bundle.js'));
 });
