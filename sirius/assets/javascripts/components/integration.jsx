5d0582ed465a6a95d06e79624d4e79e1c66951d9
bruxromuar <brux.romuar@gmail.com>
2016-04-06 03:41:52 +0800

    use generic integration components

--- sirius/assets/javascripts/components/integration.jsx
+++ sirius/assets/javascripts/components/integration.jsx
@@ -1,170 +1,116 @@
 import React from 'react';
 import _ from 'underscore';
 
-const Basecamp = React.createClass({
+const integrationTypes = {
+    basecamp: {
+        label: 'Basecamp',
+        attributes: [
+            { label: 'Address', key: 'url', type: 'url' }
+        ],
+        commands: []
+    },
+    wordpress: {
+        label: 'Wordpress',
+        attributes: [
+            { label: 'User', key: 'user', type: 'text' },
+            { label: 'Password', key: 'user', type: 'text' },
+            { label: 'Address', key: 'url', type: 'url' }
+        ],
+        commands: [
+            { label: 'Admin', callback: function() { let a = window.open(); a.opener = null; a.location = this.state.url; } },
+
+        ]
+    }
+};
+
+export default React.createClass({
+
     propTypes: {
-        integration: React.PropTypes.object,
+        id: React.PropTypes.string,
+        kind: React.PropTypes.string,
+        data: React.PropTypes.object,
         save: React.PropTypes.func,
         delete: React.PropTypes.func
     },
 
     getInitialState() {
-        let isNew = this.props.integration.get('id').charAt(0) === '?';
-        return {
-            isEditing: isNew,
-            url: this.props.integration.get('data').url
+        if (typeof integrationTypes[this.props.kind] === 'undefined') {
+            throw new Error(`Cannot generate integration component for "${this.props.kind}" without a manifest.`);
+        }
+
+        let isNew = this.props.id.charAt(0) === '?';
+        let state = { isEditing: isNew }
+
+        integrationTypes[this.props.kind].attributes.map(attr => {
+            state[attr.key] = this.props.data[attr.key];
+        });
+
+        return state;
+    },
+
+    updateInput(e) {
+        debugger;
+    },
+
+    onEdit(e) {
+        e.preventDefault();
+        this.setState({ isEditing: true });
+    },
+
+    onCancelEdit(e) {
+        e.preventDefault();
+        this.setState({ isEditing: false });
+    },
+
+    onDelete(e) {
+        e.preventDefault();
+        if (confirm('Are you sure you want to remove this integration?')) {
+          this.props.delete(this.props.id);
         }
     },
 
-  doEdit(e) {
-    e.preventDefault();
-    this.setState({ isEditing: true });
-  },
-
-  cancelEdit(e) {
-    e.preventDefault();
-    this.setState({ isEditing: false });
-  },
-
-  saveChanges(e) {
-    e.preventDefault()
-    if (this.state.url !== '') {
-      this.props.doSave(this.props.id, this.props.kind, {
-        url: this.state.url
-      });
-    } else {
-      alert('URL is required');
-    }
-  },
-
-  updateUrl(e) {
-    this.setState({ url: e.target.value });
-  },
-
-  onDelete(e) {
-    e.preventDefault();
-    if (confirm('Are you sure you want to remove this integration?')) {
-      this.props.delete(this.props.id);
-    }
-  },
-
-  render() {
-    let url, actions;
-    if (this.state.isEditing) {
-      url = <input type="url" value={this.state.url} autoFocus={this.state.isEditing} onChange={this.updateUrl} />
-      actions = <ul className="integration-actions">
-        <li><button>Save Changes</button></li>
-        <li><a href="#" onClick={this.cancelEdit}>Cancel</a></li>
-        <li><a href="#" onClick={this.onDelete}>Delete</a></li>
-      </ul>
-    } else {
-      url = <a href={this.state.url} target="_blank" rel="noreferrer noopener">{this.state.url}</a>
-      actions = <ul className="integration-actions">
-        <li><a href="#" onClick={this.doEdit}>Edit</a></li>
-      </ul>
-    }
-
-    return <div className="integration">
-      <form onSubmit={this.saveChanges}>
-        <strong className="integration-title">Basecamp</strong>
-        <dl>
-          <dt>URL</dt>
-          <dd>{url}</dd>
-        </dl>
-        {actions}
-      </form>
-    </div>
-  }
-});
-
-const Wordpress = React.createClass({
-    propTypes: {
-        integration: React.PropTypes.object,
-        save: React.PropTypes.func,
-        delete: React.PropTypes.func
-      },
-
-    getInitialState() {
-        let isNew = this.props.integration.get('id').charAt(0) === '?';
-        return {
-          isEditing: isNew,
-          user: this.props.integration.get('data').user,
-          password: this.props.integration.get('data').password,
-          url: this.props.integration.get('data').url
-        }
+    saveChanges(e) {
+        e.preventDefault();
+        // TODO: finish me
+        debugger;
     },
 
-  doEdit(e) {
-    e.preventDefault();
-    this.setState({ isEditing: true });
-  },
+    render() {
+        let integration = integrationTypes[this.props.kind];
+        var formClass, actions;
+        if (this.state.isEditing) {
+            formClass = 'integration-form editing';
+            actions = <ul className="integration-actions">
+                <li><button>Save Changes</button></li>
+                <li><a href="#" onClick={this.onCancelEdit}>Cancel</a></li>
+                <li><a href="#" onClick={this.onDelete}>Delete</a></li>
+            </ul>
+        } else {
+            formClass = 'integration-form';
+            actions = <ul className="integration-actions">
+                <li><a href="#" onClick={this.onEdit}>Edit</a></li>
+            </ul>
+        }
 
-  cancelEdit(e) {
-    e.preventDefault();
-    this.setState({ isEditing: false });
-  },
-
-  saveChanges(e) {
-    e.preventDefault()
-    this.props.doSave(this.props.id, this.props.kind, {
-      user: this.state.user,
-      password: this.state.password,
-      url: this.state.url
-    });
-  },
-
-  updateStateFromInput(e) {
-    let state = {}
-    state[e.target.dataset.key] = e.target.value;
-    this.setState(state);
-  },
-
-  render() {
-    let isEditing = this.state.isEditing;
-    let url;
-    let actions;
-
-    if (isEditing) {
-      url = <input type="url" data-key="url" value={this.state.url} onChange={this.updateStateFromInput} required />
-      actions = <ul className="integration-actions">
-        <li><button>Save Changes</button></li>
-        <li><a href="#" onClick={this.cancelEdit}>Cancel</a></li>
-        <li><a href="#">Delete</a></li>
-      </ul>
-    } else {
-      url = <a href={this.state.url} target="_blank" rel="noreferrer noopener">{this.state.url}</a>
-      actions = <ul className="integration-actions">
-        <li><button type="button">Admin</button></li>
-        <li><a href="#" onClick={this.doEdit}>Edit</a></li>
-      </ul>
+        return <div className="integration">
+            <h4 className="integration-title">{integration.label}</h4>
+            <form className={formClass} onSubmit={this.saveChanges}>
+                {integration.attributes.map(attr => {
+                    return <label key={attr.key} className="integration-form-group">
+                        {attr.label}
+                        <input
+                            type={attr.type}
+                            data-key={attr.key}
+                            value={this.state[attr.key]}
+                            className="integration-form-control"
+                            readOnly={!this.state.isEditing}
+                            onChange={this.updateInput}
+                            required />
+                    </label>
+                })}
+                {actions}
+            </form>
+        </div>
     }
 
-    return (
-      <div className="integration">
-        <form onSubmit={this.saveChanges}>
-          <strong className="integration-title">Wordpress</strong>
-          <dl className="integration-credentials">
-            <dt>User:</dt>
-            <dd>
-              <input type="text" data-key="user" value={this.state.user} readOnly={!isEditing} autoFocus={isEditing} onChange={this.updateStateFromInput} required />
-            </dd>
-            <dt>Password:</dt>
-            <dd>
-              <input type="text" data-key="password" value={this.state.password} readOnly={!isEditing} onChange={this.updateStateFromInput} required />
-            </dd>
-            <dt>URL:</dt>
-            <dd>
-              {url}
-            </dd>
-          </dl>
-          {actions}
-        </form>
-      </div>
-    )
-  }
 });
-
-export default {
-  Basecamp,
-  Wordpress: Wordpress
-}
--- sirius/assets/javascripts/components/integrations.jsx
+++ sirius/assets/javascripts/components/integrations.jsx
@@ -36,41 +36,43 @@ export default React.createClass({
   },
 
     render: function() {
-        let integrations = this.state.items.map((item) => {
-            let identifier = item.get('kind').charAt(0).toUpperCase() + item.get('kind').slice(1);
-            let Component = Integration[identifier];
-            return <Component key={item.get('id')} integration={item} save={this.saveIntegration} delete={this.deleteIntegration} />
-        });
-
-    if (this.state.isFetching) {
-      return (
-        <div className="project-integrations">
-          <header className="page-header">
-            <h3>Integrations</h3>
-          </header>
-          <div className="preloader">Loading Integrations&hellip;</div>
-        </div>
-      );
-    } else {
-      return (
-        <div className="project-integrations">
-          <header className="page-header">
-            <h3>Integrations</h3>
-            <form className="form-h" onSubmit={this.addIntegration}>
-              <select id="add-integration-kind" defaultValue="">
-                <option value="">Select Integration&hellip;</option>
-                <option value="basecamp">Basecamp</option>
-                <option value="sftp">SFTP</option>
-                <option value="wordpress">Wordpress</option>
-              </select>
-              <button onClick={this.addIntegration}>Add Integration</button>
-            </form>
-          </header>
-          {integrations}
-        </div>
-      );
-    }
-  },
+        if (this.state.isFetching) {
+          return (
+            <div className="project-integrations">
+              <header className="page-header">
+                <h3>Integrations</h3>
+              </header>
+              <div className="preloader">Loading Integrations&hellip;</div>
+            </div>
+          );
+        } else {
+          return (
+            <div className="project-integrations">
+              <header className="page-header">
+                <h3>Integrations</h3>
+                <form className="form-h" onSubmit={this.addIntegration}>
+                  <select id="add-integration-kind" defaultValue="">
+                    <option value="">Select Integration&hellip;</option>
+                    <option value="basecamp">Basecamp</option>
+                    <option value="sftp">SFTP</option>
+                    <option value="wordpress">Wordpress</option>
+                  </select>
+                  <button onClick={this.addIntegration}>Add Integration</button>
+                </form>
+              </header>
+              {this.state.items.map(item => {
+                return <Integration
+                    key={item.get('id')}
+                    id={item.get('id')}
+                    kind={item.get('kind')}
+                    data={item.get('data')}
+                    save={this.saveIntegration}
+                    delete={this.deleteIntegration} />
+              })}
+            </div>
+          );
+        }
+    },
 
   addIntegration(e) {
     e.preventDefault();
