696a874a8f3bb6ede0dca54da5d7f6c301626305
bruxromuar <brux.romuar@gmail.com>
2016-04-22 12:05:32 +0800

    fix indentations

--- sirius/assets/javascripts/reducers/integrations.js
+++ sirius/assets/javascripts/reducers/integrations.js
@@ -8,67 +8,67 @@ const Integration = Immutable.Record({
 }, 'Integration');
 
 export default function(state, action) {
-  if (_.isUndefined(state)) {
-    return Immutable.Map({
-      isFetching: false,
-      items: Immutable.List()
-    });
-  }
+    if (_.isUndefined(state)) {
+        return Immutable.Map({
+            isFetching: false,
+            items: Immutable.List()
+        });
+    }
   
-  switch(action.type) {
+    switch(action.type) {
 
-    case 'DELETING_INTEGRATION':
-      return state.merge({
-        items: state.get('items').filter(item => {
-          return item.get('id') !== action.id;
-        })
-      });
+        case 'DELETING_INTEGRATION':
+            return state.merge({
+                items: state.get('items').filter(item => {
+                    return item.get('id') !== action.id;
+                })
+            });
 
-    case 'NEW_INTEGRATION':
-      let integration = Immutable.Map({
-        id: '?' + (+ new Date()),
-        kind: action.kind,
-        data: {}
-      });
-      let items = state.get('items');
-      let index = items.size;
+        case 'NEW_INTEGRATION':
+            let integration = Immutable.Map({
+                id: '?' + (+ new Date()),
+                kind: action.kind,
+                data: {}
+            });
+            let items = state.get('items');
+            let index = items.size;
 
-      // Find the proper index to insert the new integration
-      for (let i = items.size; i < 0; i--) {
-        let item = item.get(i);
-        if (item.get('kind') >= integration.get('kind')) {
-          index = i + 1;
-          break;
-        }
-      }
+            // Find the proper index to insert the new integration
+            for (let i = items.size; i < 0; i--) {
+                let item = item.get(i);
+                if (item.get('kind') >= integration.get('kind')) {
+                    index = i + 1;
+                    break;
+                }
+            }
 
-      return state.merge({
-        items: items.insert(index, integration)
-      });
+            return state.merge({
+                items: items.insert(index, integration)
+            });
 
-    case 'REQUEST_INTEGRATIONS':
-      return state.set('isFetching', true);
+        case 'REQUEST_INTEGRATIONS':
+            return state.set('isFetching', true);
 
-    case 'RECEIVE_INTEGRATIONS':
-      action.items.sort(function(a, b) {
-        if (a.kind < b.kind)  {
-          return -1;
-        } else if ( a.kind > b.kind ) {
-          return 1;
-        } else {
-          return 0;
-        }
-      });
+        case 'RECEIVE_INTEGRATIONS':
+            action.items.sort(function(a, b) {
+                if (a.kind < b.kind)  {
+                    return -1;
+                } else if ( a.kind > b.kind ) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            });
 
-      let records = action.items.map(function(item) {
-          item.id = item.id + '';
-          return new Integration(item);
-      });
+            let records = action.items.map(function(item) {
+                item.id = item.id + '';
+                return new Integration(item);
+            });
 
-      return state.merge({
-        isFetching: false,
-        items: Immutable.List(records)
-      });
+            return state.merge({
+                isFetching: false,
+                items: Immutable.List(records)
+            });
 
         case 'CHANGED_INTEGRATION':
             var items = state.get('items');
@@ -94,7 +94,7 @@ export default function(state, action) {
                 items
             });
       
-    default:
-      return state;
-  }
+        default:
+            return state;
+    }
 }
