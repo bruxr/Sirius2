d3080c62543fe07d560b0ae6849df0b5b46e7332
bruxromuar <brux.romuar@gmail.com>
2016-05-11 11:03:06 +0800

    fix creds for retrieving file

--- sirius/storage.py
+++ sirius/storage.py
@@ -40,18 +40,20 @@ def get_url(path, ttl=15):
     cpath = '/' + bucket + '/' + path
 
     data = []
-    data.append('GET')      # Method
-    data.append('')         # MD5 digest value
-    data.append('')         # Content-type
-    data.append(expiry)     # Expiry date
-    data.append(cpath)      # Path to the resource
+    data.append('GET')          # Method
+    data.append('')             # MD5 digest value
+    data.append('')             # Content-type
+    data.append(str(expiry))    # Expiry date
+    data.append(cpath)          # Path to the resource
+    data_str = "\n".join(data)
+    print(type(data_str))
     
-    signing_key_name, signature = app_identity.sign_blob(data.join("\n"))
+    signing_key_name, signature = app_identity.sign_blob(str(data_str))
     
     url = 'https://storage.googleapis.com'
     url += cpath
     url += '?GoogleAccessId=' + app_identity.get_service_account_name()
-    url += '&Expires=' + expiry
+    url += '&Expires=' + str(expiry)
     url += '&Signature=' + urllib.quote_plus(base64.b64encode(signature))
     
     return url
