e0c0ddcfeb6c3265a2a09ecfab36db44155eea9b
bruxromuar <brux.romuar@gmail.com>
2016-05-24 01:30:41 +0800

    expand api client

---
+++ sirius/assets/javascripts/api.js
@@ -0,0 +1,49 @@
+export default function get(url, opts) {
+
+    const headers = Object.assign({
+        'Accept': 'application/json'
+    }, opts.headers);
+    const options = Object.assign({
+        method: 'GET',
+        mode: 'same-origin',
+        credentials: 'same-origin',
+        headers
+    }, opts);
+  
+    return new Promise((resolve, reject) => {
+        fetch(url, options).then(resp => {
+            if (resp.ok) {
+                resolve(resp.json());
+            } else {
+                reject(resp);
+            }
+        });
+    });
+}
+
+export function post(url, opts) {
+    const options = Object.assign({
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        }
+    }, opts);
+    return get(url, options);
+}
+
+export function patch(url, opts) {
+    const options = Object.assign({
+        method: 'PATCH',
+        headers: {
+            'Content-type': 'application/json'
+        }
+    }, opts);
+    return get(url, options);
+}
+
+export function delet(url, opts) {
+    const options = Object.assign({
+        method: 'DELETE'
+    }, opts);
+    return get(url, options);
+}
