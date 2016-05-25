52dea7882af252b1ee463b61023926ef03583025
bruxromuar <brux.romuar@gmail.com>
2016-05-25 14:24:42 +0800

    do not render if there is no repo

--- sirius/assets/javascripts/components/repo.jsx
+++ sirius/assets/javascripts/components/repo.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
+import _ from 'underscore'
+import React from 'react'
+import moment from 'moment'
 
 const Repo = (props) => {
-    console.log(props)
-    return (<section className="project-section project-section_repo">
-        <h2>Repository</h2>
-        <dl>
-            <dt>URL</dt>
-            <dd><a href="#">bitbucket.com/bruxr/Sirius</a></dd>
-            <dt>Last Commit</dt>
-            <dd><a href="#">5ac32a</a> 30 minutes ago</dd>
-        </dl>
-    </section>)
+    if (_.isUndefined(props.repo)) {
+        return <noscript />
+    }
+
+    return (
+        <section className="project-section project-section_repo">
+            <h2>Repository</h2>
+            <dl>
+                <dt>URL</dt>
+                <dd><a href="#">bitbucket.com/bruxr/Sirius</a></dd>
+                <dt>Last Commit</dt>
+                <dd><a href="#">5ac32a</a> 30 minutes ago</dd>
+            </dl>
+        </section>
+    )
 }
 
 Repo.propTypes = {
-    isEditing: React.PropTypes.boolean,
+    isEditing: React.PropTypes.bool,
     repo: React.PropTypes.shape({
-        
+        url: React.PropTypes.string,
+        isDeploying: React.PropTypes.bool,
+        lastCommit: React.PropTypes.string,
+        lastCommitDate: React.PropTypes.instanceOf(moment)
     })
 }
 
