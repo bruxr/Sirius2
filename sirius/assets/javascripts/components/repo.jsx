0367a2a6b4b50545f0eaf1a148e273737fe4e642
bruxromuar <brux.romuar@gmail.com>
2016-05-30 14:13:46 +0800

    split conditional display into two functions

--- sirius/assets/javascripts/components/repo.jsx
+++ sirius/assets/javascripts/components/repo.jsx
@@ -1,56 +1,59 @@
-import _ from 'underscore'
 import React from 'react'
 import moment from 'moment'
 
 const Repo = (props) => {
-    if (_.isUndefined(props.repo)) {
+    if (typeof props.repo === 'undefined') {
         return <noscript />
     }
 
-    if (props.isEditing) {
-        let repos = []
-        if (props.isFetchingHosted) {
-            repos.push(<option key="norepos">Fetching list...</option>)
-        } else {
-            repos.push(<optgroup key="bitbucket" label="BitBucket" />)
-            _.each(props.hosted.bitbucket, (repo, index) => {
-                repos.push(<option key={`bitbucket-${index}`} value={repo.url}>{repo.name}</option>)
-            });
+    return (
+        <section className="project-section project-section_repo">
+            <h2>Repository</h2>
+            {props.isEditing ? repoForm(props) : repoView(props)}
+        </section>
+    )
+}
 
-            repos.push(<optgroup key="github" label="GitHub" />)
+function repoForm(props) {
+    let repos = []
+    if (props.isFetchingHosted) {
+        repos.push(<option key="norepos">Fetching list...</option>)
+    } else {
+        repos.push(<optgroup key="bitbucket" label="BitBucket" />)
+        for (let index = 0; index < props.hosted.bitbucket.length; index++) {
+            const repo = props.hosted.bitbucket[index]
+            repos.push(<option key={`bitbucket-${index}`} value={repo.url}>{repo.name}</option>)
         }
 
-        return (
-            <section className="project-section project-section_repo">
-                <h2>Repository</h2>
-                <form
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        const repo = document.getElementById('repo-url').value
-                        props.onSetRepo(repo)
-                    }}
-                >
-                    <fieldset className="form-group">
-                        <label htmlFor="repo-url">Repository</label>
-                        <select id="repo-url" className="form-control" disabled={props.isFetchingHosted}>{repos}</select>
-                    </fieldset>
-                    <button className="btn btn-primary" disabled={props.isFetchingHosted}>Save Changes</button>
-                </form>
-            </section>
-        )
-    } else {
-        return (
-            <section className="project-section project-section_repo">
-                <h2>Repository</h2>
-                <dl>
-                    <dt>URL</dt>
-                    <dd><a href={props.repo.url}>{props.repo.url}</a></dd>
-                    <dt>Last Commit</dt>
-                    <dd><a href="#">COMMIT GOES HERE</a> COMMIT DATE GOES HERE</dd>
-                </dl>
-            </section>
-        )
+        repos.push(<optgroup key="github" label="GitHub" />)
     }
+
+    return (
+        <form
+            onSubmit={(e) => {
+                e.preventDefault();
+                const repo = document.getElementById('repo-url').value
+                props.onSetRepo(repo)
+            }}
+        >
+            <fieldset className="form-group">
+                <label htmlFor="repo-url">Repository</label>
+                <select id="repo-url" className="form-control" disabled={props.isFetchingHosted}>{repos}</select>
+            </fieldset>
+            <button className="btn btn-primary" disabled={props.isFetchingHosted}>Save Changes</button>
+        </form>
+    )
+}
+
+function repoView(props) {
+    return (
+        <dl>
+            <dt>URL</dt>
+            <dd><a href={props.repo.url}>{props.repo.url}</a></dd>
+            <dt>Last Commit</dt>
+            <dd><a href="#">COMMIT GOES HERE</a> COMMIT DATE GOES HERE</dd>
+        </dl>
+    )
 }
 
 Repo.propTypes = {
